4fa652220a70f5ca09be86559d24f0e5c79e0bf4
Kowyno <misterpngn@gmail.com>
2025-07-03 22:30:51 -0400

    Article: "Show", "New", "Create" added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "/articles", to: "articles#index"
+  get "/articles/new", to: "articles#new"
+  get "/articles/:id", to: "articles#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,4 +14,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :articles
+
+  root "articles#index"
 end
